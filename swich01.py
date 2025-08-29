533d71882c87d6c0640e8e18dbfc61cde067687d
marvin_oparada <marvin.parada@ugb.edu.sv>
2025-08-29 12:25:27 -0600

    new

---
+++ swich01.py
@@ -0,0 +1,11 @@
+hora = 19
+match hora:
+    case 7|8|9:print("Ve y desayuna")
+    case 11|12|13:print("Ve a tu almuerzo")
+    case 17|18|19:print("Ve a cenar")
+    case _:print("No toca comer")
+
+
+
+
+
